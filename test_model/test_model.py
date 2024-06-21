97f7a81a8dfed3696b4952e48e81b6aad70f063b
wq-win <643797037@qq.com>
2024-06-21 21:04:40 +0800

    update test_model

--- test_model/test_model.py
+++ test_model/test_model.py
@@ -14,10 +14,14 @@ os.sys.path.insert(0, parentdir)
 from pretrain import pretrain_save_data_V1
 
 
-def oa_to_a(oa):
-    oa[np.array([1, 4, 7, 10])] -= 0.67
-    oa[np.array([2, 5, 8, 11])] -= -1.25
-    return oa
+TIMESTEP = 1 / 30
+DISPLACEMENT_RATE = 1
+
+def oma_to_pma(oma):
+    oma[np.array([0, 6])] = -oma[np.array([0, 6])]
+    oma[np.array([1, 4, 7, 10])] -= 0.6
+    oma[np.array([2, 5, 8, 11])] -= -0.66
+    return oma
 
 def main():
     env = env_builder.build_imitation_env(motion_files=["motion_imitation/data/motions/dog_pace.txt"],
@@ -26,19 +30,19 @@ def main():
                                         enable_randomizer=False,
                                         enable_rendering=True)
     o = env.reset()
-    oa = o[12:24]
-    oa = oa_to_a(oa)
-    i = 0
+    o = torch.tensor(o, dtype=torch.float32)
+    test_model = pretrain_save_data_V1.Net(12, 12)
+    test_model.load_state_dict(torch.load('pretrain_model/save_data_V5_model_06_21_11_06_43.pkl', map_location=torch.device('cpu')))
+    oma = o[48:60]
+    pma = oma_to_pma(oma)
+    displacement = test_model(pma)
+    # displacement[np.array([0, 6])] = -displacement[np.array([0, 6])]
+    action = oma + displacement * TIMESTEP * DISPLACEMENT_RATE
+
     while True:
-        o, r, d, _ = env.step(oa)
-        oa = o[12:24]
-        print(o[48:60])
-        oa = oa_to_a(oa)
-        if i == 500:
-            env.reset()
-            i = 0
-        i += 1
-        print(i)
+        o, r, d, _ = env.step()
+        
+
     
 if __name__ == '__main__':
     main()
